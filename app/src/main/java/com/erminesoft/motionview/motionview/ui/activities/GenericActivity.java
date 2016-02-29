9bc632ad2e184618904360a3e689c8ed2153496b
vkraevskiy <vladkraevskii@gmail.com>
2016-02-29 12:22:23 +0200

    set portrait orientation for app

--- app/src/main/java/com/erminesoft/motionview/motionview/ui/activities/GenericActivity.java
+++ app/src/main/java/com/erminesoft/motionview/motionview/ui/activities/GenericActivity.java
@@ -1,6 +1,7 @@
 package com.erminesoft.motionview.motionview.ui.activities;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -19,6 +20,8 @@ public abstract class GenericActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mGoogleClientFacade = getMVApplication().getGoogleClientFacade();
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public final MVApplication getMVApplication() {
